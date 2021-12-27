9015449375cbd179d780993db43d95c718644a3c
石井将司 <optacos@gmail.com>
2021-12-28 01:22:49 +0900

    ADD: 作成画面

---
+++ backend/resources/js/components/TaskCreateComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <fiv class="row justify-content-center">
+            <div class="col-sm-6">
+                <form action="form-group row">
+                    <div class="form-group row">
+                        <label for="title" class="col-sm-3 col-form-label">Title</label>
+                        <input type="text" class="cols-sm-9 form-control" id="title">
+                    </div>
+                    <div class="form-group row">
+                        <label for="content" class="col-sm-3 col-form-label">Content</label>
+                        <input type="text" class="cols-sm-9 form-control" id="title">
+                    </div>
+                    <div class="form-group row">
+                        <label for="person-in-charge" class="clo-sm-3 col-form-label">Person In Charge</label>
+                        <input type="text" class="cols-sm-9 form-control" id="person-in-charge">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </fiv>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
