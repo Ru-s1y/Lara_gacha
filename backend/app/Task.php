e09edb320b0b52ad960ea46f3e90482eb77cc2fd
石井将司 <optacos@gmail.com>
2021-12-28 22:49:31 +0900

    ADD: タスクモデル作成

---
+++ backend/app/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'person_in_change'
+    ];
+
+    public function getTaskAll() {
+        return $this->all();
+    }
+}
