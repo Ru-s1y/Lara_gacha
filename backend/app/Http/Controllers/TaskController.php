eba547929d14c914a0083062e082efa3f2fb5a49
石井将司 <optacos@gmail.com>
2021-12-28 22:50:06 +0900

    ADD: 一覧と詳細API追加

---
+++ backend/app/Http/Controllers/TaskController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public $task;
+
+    public function __construct(Task $task) {
+        $this->task = $task;
+    }
+
+    public function index() {
+        return $this->task->getTaskAll();
+    }
+
+    public function show(Task $task) {
+        return $task;
+    }
+}
