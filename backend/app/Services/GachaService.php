e1b65256685c9aec6fb4a635c0dbc0add0105ce1
石井将司 <optacos@gmail.com>
2021-12-30 00:31:02 +0900

    UPDATE: ガチャ内容をconfigに一時待避

--- backend/app/Services/GachaService.php
+++ backend/app/Services/GachaService.php
@@ -13,17 +13,8 @@ class GachaService
 
     public function __construct()
     {
-        // マスターにおこう
-        $this->characters = array(
-            "SSR"   => ["5001", "5002", "5003", "5004"],
-            "SR"    => ["4001", "4002", "4003", "4004", "4005", "4006"],
-            "R"     => ["3001", "3002", "3003", "3004", "3005", "3006", "3007", "3008", "3009", "3010"]
-        );
-        $this->gradeProbability = array(
-            "SSR"   => 300,
-            "SR"    => 1200,
-            "R"     => 8500,
-        );
+        $this->characters = GachaConst::CHARACTERS;
+        $this->gradeProbability = GachaConst::GRADE_PROBABILITY;
     }
 
     public function getGachaInfo() {
