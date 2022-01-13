64f5dec8ba55154ef72e70c74c54ea827796b2c5
石井将司 <optacos@gmail.com>
2022-01-13 16:22:39 +0900

    UPDATE: ヘッダーのデザイン修正

--- backend/resources/js/components/HeaderComponent.vue
+++ backend/resources/js/components/HeaderComponent.vue
@@ -1,18 +1,37 @@
 <template>
     <div class="container-fluid bg-dark mb-3">
         <div class="container">
-            <nav class="navbar navbar-dark">
-                <router-link v-bind:to="{name: 'home'}">
-                    <span class="navbar-brand mb-0 h1">Vue Laravel SPA</span>
-                </router-link>
-                <div>
-                    <router-link v-bind:to="{name: 'task.list'}">
-                        <button class="btn btn-success">List</button>
-                    </router-link>
-                    <router-link v-bind:to="{name: 'task.create'}">
-                        <button class="btn btn-success">Add</button>
-                    </router-link>
-                    <button class="btn btn-danger" v-on:click="logout">Logout</button>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <span class="navbar-brand">Vue Laravel SPA</span>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <router-link v-bind:to="{name: 'home'}">
+                                <span class="nav-link">Home</span>
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link v-bind:to="{name: 'task.list'}">
+                                <span class="nav-link">Task</span>
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" >Pricing</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <span class="nav-link dropdown-toggle"  id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{ this.$store.getters.getUsername }}
+                            </span>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <span class="dropdown-item" v-on:click="logout">Logout</span>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </div>
