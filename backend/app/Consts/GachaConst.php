235dd8ef93ae8d94515c37c4c658ea84cdd20497
石井将司 <optacos@gmail.com>
2022-01-14 10:49:32 +0900

    UPDATE: マスターの更新

--- backend/app/Consts/GachaConst.php
+++ backend/app/Consts/GachaConst.php
@@ -18,4 +18,104 @@ class GachaConst
         "R"     => ["3001", "3002", "3003", "3004", "3005", "3006", "3007", "3008", "3009", "3010"]
     ];
 
+    const GENDER = [
+        [
+            "name_en" => "Unknown",
+            "name_jp" => "不明"
+        ],
+        [
+            "name_en" => "Male",
+            "name_jp" => "男"
+        ],
+        [
+            "name_en" => "Female",
+            "name_jp" => "女"
+        ],
+    ];
+
+    const ELEMENTS = [
+        [
+            "name_en" => "Fire",
+            "name_jp" => "火"
+        ],
+        [
+            "name_en" => "Water",
+            "name_jp" => "水"
+        ],
+        [
+            "name_en" => "Wind",
+            "name_jp" => "風"
+        ],
+        [
+            "name_en" => "Earth",
+            "name_jp" => "土"
+        ],
+        [
+            "name_en" => "Thunder",
+            "name_jp" => "雷"
+        ],
+        [
+            "name_en" => "Ice",
+            "name_jp" => "氷"
+        ]
+    ];
+
+    const RACES = [
+        [
+            "name_en" => "Human",
+            "name_jp" => "人族"
+        ],
+        [
+            "name_en" => "Beast",
+            "name_jp" => "獣人族"
+        ],
+        [
+            "name_en" => "Fairy",
+            "name_jp" => "妖精族"
+        ],
+        [
+            "name_en" => "Dwarf",
+            "name_jp" => "鉱人族"
+        ],
+        [
+            "name_en" => "Daemon",
+            "name_jp" => "悪魔族"
+        ]
+    ];
+
+    const ROLES = [
+        [
+            "name_en" => "Adventurer",
+            "name_jp" => "冒険者"
+        ],
+        [
+            "name_en" => "Warrior",
+            "name_jp" => "戦士"
+        ],
+        [
+            "name_en" => "Mage",
+            "name_jp" => "魔道士"
+        ],
+        [
+            "name_en" => "Cleric",
+            "name_jp" => "聖職者"
+        ],
+        [
+            "name_en" => "Hunter",
+            "name_jp" => "狩人"
+        ],
+        [
+            "name_en" => "Scout",
+            "name_jp" => "斥候"
+        ],
+        [
+            "name_en" => "Knight",
+            "name_jp" => "騎士"
+        ],
+        [
+            "name_en" => "Astrologer",
+            "name_jp" => "占星術士"
+        ],
+    ];
+
 }
